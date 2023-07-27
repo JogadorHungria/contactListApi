0cb2ad9859ba0957b1b19794454aff9110b72184
joilson <joilsonhungrianv@gmail.com>
2023-07-27 19:58:58 -0300

    feat: finalizing routs contacts

--- src/routers/contacts.router.ts
+++ src/routers/contacts.router.ts
@@ -1,13 +1,34 @@
 import { Router } from "express";
+import contactsControler from "../controller/contacts.controller";
+import userMiddlewares from "../middlewares/user.middlewares";
+import contactSchemas from "../schemas/contact.schemas";
+import validateSchema from "../middlewares/validateSchema.middlewares";
+import contactMiddlewares from "../middlewares/contact.middlewares";
 
 const contacts = Router();
 
-contacts.post("");
+contacts.use(userMiddlewares.verifyToken);
 
-contacts.get("");
+contacts.post(
+  "",
+  validateSchema(contactSchemas.request),
+  contactsControler.contactCreation
+);
 
-contacts.patch("/:id");
+contacts.get("", contactsControler.contactRead);
 
-contacts.delete("/:id");
+contacts.patch(
+  "/:id",
+  validateSchema(contactSchemas.requestPartial),
+  contactMiddlewares.verifyContactExists,
+  contactMiddlewares.verifyUserAndOwner,
+  contactsControler.contactUpdate
+);
+
+contacts.delete(
+  "/:id",
+  contactMiddlewares.verifyContactExists,
+  contactsControler.contactDelete
+);
 
 export default contacts;
