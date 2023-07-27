12266b75096b3f0f4bdef64371240fb269003775
joilson <joilsonhungrianv@gmail.com>
2023-07-27 19:31:26 -0300

    feat(service): adding listAllUser in userServices

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,9 +1,11 @@
 import createUser from "./createUser.service";
 import deleteUser from "./deletUser.service";
+import listAllUser from "./findByAllUser.service";
 
 const userServices = {
   createUser,
   deleteUser,
+  listAllUser,
 };
 
 export default userServices;
